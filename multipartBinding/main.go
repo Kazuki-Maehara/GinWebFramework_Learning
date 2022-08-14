ca9574df0b3c4c97e278c82eb4d76147bcb4bb65
Kazuki Maehara <treize.citron13@gmail.com>
2022-08-14 11:14:25 +0900

    added an example

---
+++ multipartBinding/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"mime/multipart"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ProfileForm struct {
+	Name   string                `form:"name" binding:"required"`
+	Avatar *multipart.FileHeader `form:"avatar" binding:"required"`
+
+	// or for multiple files
+	// Avatars []*multipart.FileHeader `form:"avatar" binding:"required"`
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/profile", func(c *gin.Context) {
+		var form ProfileForm
+
+		if err := c.ShouldBind(&form); err != nil {
+			c.String(http.StatusBadRequest, "bad request")
+			return
+		}
+
+		err := c.SaveUploadedFile(form.Avatar, form.Avatar.Filename)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "nknown error")
+			return
+		}
+
+		c.String(http.StatusOK, "ok")
+	})
+
+	router.Run(":8080")
+}
