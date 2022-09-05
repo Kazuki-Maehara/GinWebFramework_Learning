eb5db295361334cfe409cd708c1551e6741fb533
Kazuki Maehara <treize.citron13@gmail.com>
2022-09-05 23:10:27 +0900

    added an example

---
+++ customLogFormat/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.New()
+
+	// LoggerWithFormatter middleware will write the logs to gin.DefaultWriter
+	// By default gin.DefaultWriter = os.Stdout
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		// custom format
+		return fmt.Sprintf(
+			"%s - [%s] \"%s %s %s %d %d '%s' %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+
+	router.Use(gin.Recovery())
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "ping ping ping!")
+	})
+
+	router.Run(":8080")
+
+}
