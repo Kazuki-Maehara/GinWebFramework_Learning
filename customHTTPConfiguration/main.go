663dc75da174b074bfcbb41c33bfa127da4162c7
Kazuki Maehara <treize.citron13@gmail.com>
2022-09-06 20:11:19 +0900

    added an example

---
+++ customHTTPConfiguration/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	// router := gin.Default()
+	// http.ListenAndServe(":8080", router)
+
+	router := gin.Default()
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
+
+}
